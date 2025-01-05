3e5198809090288bd9e83746108886dcfd12369a
A-Iok <firecerambycidae625@gmail.com>
2025-01-05 20:26:07 +0900

    円グラフを表示する

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,61 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { Cell, Pie, PieChart } from 'recharts';
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  /** 円グラフに登録するデータ */
+  let data = [
+    {
+      index: 0,
+      name: 'データ1',
+      value: 10,
+    }, {
+      index: 1,
+      name: 'データ2',
+      value: 2,
+    }, {
+      index: 2,
+      name: 'データ3',
+      value: 3,
+    }, {
+      index: 3,
+      name: 'データ4',
+      value: 4,
+    }, {
+      index: 4,
+      name: 'データ5',
+      value: 5,
+    }
+  ];
+
+  /** 円グラフの各要素の色 */
+  const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", '#afeeee'];
+
+  /** 円グラフのラベル定義 */
+  const label = ({ name }: any) => {
+    return name;
+  }
+
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <PieChart width={700} height={700} className='margin10px'>
+          <Pie data={data} dataKey="value" cx="50%" cy="50%" outerRadius={200} fill="#82ca9d" label={label} > {
+            data.map((entry, index) =>
+              (<Cell key={`cell-${index}`} fill={COLORS[index]} />)
+            )
+          } </Pie>
+        </PieChart>
       </div>
-      <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
